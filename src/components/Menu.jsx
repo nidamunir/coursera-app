b7713cb954e2faacb01a9c69f8066d2c6f55e872
nidamunir <nidamunir47@outlook.com>
2019-04-22 21:53:35 +0500

    fetch handling errors

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -19,7 +19,7 @@ function RenderMenuItem({ dish, onClick }) {
 }
 
 const Menu = (props) => {
-	const menu = props.dishes.dishes.map((dish) => {
+	const menu = props.dishes.map((dish) => {
 		return (
 			<div className="col-12 col-md-5 m-1" key={dish.id}>
 				<RenderMenuItem dish={dish} onClick={props.onClick} />
